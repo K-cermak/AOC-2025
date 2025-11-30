7e50d399a70b46ad6640273518b9244f8f8708e8
K-cermak <karel4cermak@seznam.cz>
2025-11-30 23:43:13 +0100

    first commit

---
+++ day01/01-1.py
@@ -0,0 +1,8 @@
+def main():
+    with open("day01/01-input.txt", "r") as file:
+        data = file.readlines()
+        data = [line.rstrip() for line in data]
+
+
+if __name__ == "__main__":
+    main()
---
+++ template.py
@@ -0,0 +1,9 @@
+def main():
+    with open("day1X/1X-input.txt", "r") as file:
+        data = file.readlines()
+        data = [line.rstrip() for line in data]
+
+
+
+if __name__ == "__main__":
+    main()

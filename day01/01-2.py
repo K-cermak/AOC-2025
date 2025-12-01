bfd19911f36df244ee516fe8fa540001e8de7d9f
K-cermak <karel4cermak@seznam.cz>
2025-12-01 08:23:59 +0100

    feat: day 01

--- day01/01-2.py
+++ day01/01-2.py
@@ -3,21 +3,28 @@ def main():
         data = file.readlines()
         data = [line.rstrip() for line in data]
 
-
     curr = 50
     zeros = 0
 
     for line in data:
         if line[0] == "L":
+            if curr == 0:
+                zeros -= 1
+
             curr -= int(line[1:])
+            zeros -= curr // 100
+
+            if curr % 100 == 0:
+                zeros += 1
+
         elif line[0] == "R":
             curr += int(line[1:])
+            zeros += curr // 100
 
         curr %= 100
-        if curr == 0:
-            zeros += 1
-    
+
     print(zeros)
+
 
 if __name__ == "__main__":
     main()
